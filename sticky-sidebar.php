5a05216709061ec9df6f0acfa5ee24667dbb2fd3
John <john@john-VirtualBox.(none)>
2014-03-14 23:26:48 +0000

    Add facebook share into sticky

--- sticky-sidebar.php
+++ sticky-sidebar.php
@@ -24,8 +24,16 @@
             <p>Descuentos si te apuntas a más de un taller. Si tu empresa tiene necesidades formativas también podemos hablar para ofreceros un paquete de formación adaptado a lo que necesitáis. Contáctanos y te damos más detalles.</p>
         </div>
 
-        <div>
-            Me gusta / Compartir
-        </div>
+            <div id="fb-root"></div>
+            <script>(function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = "//connect.facebook.net/es_ES/all.js#xfbml=1&appId=140978915248";
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));</script>
+            <div class="share">
+                <div class="fb-like" data-href="http://www.lanavenodriza.com" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
+            </div>
     </div>                     
 </div>
